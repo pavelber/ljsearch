20f35152285a83fff8db53322ecef3236921aebf
pavelber <pavelber@salespredict.com>
2015-10-06 20:08:21 +0300

    ui

--- src/main/groovy/org/ljsearch/controllers/JournalsController.groovy
+++ src/main/groovy/org/ljsearch/controllers/JournalsController.groovy
@@ -1,26 +1,28 @@
 package org.ljsearch.controllers
 
+import groovy.json.JsonBuilder
 import org.ljsearch.entity.IJournalRepository
 import org.ljsearch.services.Downloading
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 /**
  * Created by Pavel on 10/5/2015.
  */
 @Controller
-class GetOldController {
-    @Autowired
-    ApplicationContext context
+class JournalsController {
+
     @Autowired
     IJournalRepository repo
 
 
-    @RequestMapping("/get")
-    def get(){
-        def downloader = context.getBean(Downloading.class,repo.findOne("tourism_il"))
-        downloader.run()
+    @RequestMapping("/journals")
+    @ResponseBody String get() {
+        def journals = repo.findAll()
+        journals.each {it.user = null}
+        return   new JsonBuilder( journals).toPrettyString()
     }
 }
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,24 +1,40 @@
-var app =  angular.module('app',[]);
+var app = angular.module('app', []);
 
-app.filter('trustAsHtml', function($sce) {
-    return function(html) {
+app.filter('trustAsHtml', function ($sce) {
+    return function (html) {
         return $sce.trustAsHtml(html);
     };
 });
 
-app.controller('SearchCtrl', function($scope, $http) {
+app.controller('SearchCtrl', function ($scope, $http) {
     $scope.url = '/search'; // The url of our search
     $scope.poster = '';
     $scope.journal = '';
 
+    $http.get("/journals").
+        success(function (data, status) {
+            $scope.jorunalsstatus = status;
+            $scope.journals = data;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].last != null) {
+                    data[i].formateddate = moment(data[i].last).format("MMMM Do YYYY");
+                }
+            }
+        })
+        .
+        error(function (data, status) {
+            $scope.journals = data || "Request failed";
+            $scope.jorunalsstatus = status;
+        });
+
     // The function that will be executed on button click (ng-click="search()")
-    $scope.search = function() {
+    $scope.search = function () {
 
         // Create the http post request
         // the data holds the keywords
         // The request is a JSON request.
-        $http.get($scope.url+"?journal="+$scope.journal+"&term="+$scope.keywords+"&poster="+$scope.poster).
-            success(function(data, status) {
+        $http.get($scope.url + "?journal=" + $scope.journal + "&term=" + $scope.keywords + "&poster=" + $scope.poster).
+            success(function (data, status) {
                 $scope.status = status;
                 $scope.data = data;
                 $scope.result = data; // Show result from server in our <pre></pre> element
@@ -29,7 +45,7 @@ app.controller('SearchCtrl', function($scope, $http) {
                 }
             })
             .
-            error(function(data, status) {
+            error(function (data, status) {
                 $scope.data = data || "Request failed";
                 $scope.status = status;
             });
