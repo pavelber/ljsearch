72f1290d0b97e39f7dc149f8eab06a8c26463321
pavelber <pavelber@salespredict.com>
2015-10-07 08:18:53 +0300

    war

--- src/main/groovy/org/ljsearch/Application.groovy
+++ src/main/groovy/org/ljsearch/Application.groovy
@@ -2,6 +2,8 @@ package org.ljsearch
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableAsync
@@ -13,8 +15,13 @@ import org.springframework.scheduling.annotation.EnableAsync
 @ComponentScan
 @EnableJpaRepositories(basePackages = ["org.ljsearch"])
 @EnableAsync
-public class Application {
+public class Application extends SpringBootServletInitializer  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+    // Used when deploying to a standalone servlet container
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
